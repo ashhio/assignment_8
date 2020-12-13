18404f5ebb9500a5fdfe3ec29b1a845ad9dae1a2
Akshaya Madhavan <akshayam@Akshayas-MacBook-Pro.local>
2020-12-12 22:25:58 -0600

    added comments and formmated code

--- project1/Assets/scripts/animation.js
+++ project1/Assets/scripts/animation.js
@@ -1,19 +1,23 @@
-
+/*
+creates parallax scrolling background using Greensock and ScrollTrigger
+referenced and modified from ScrollTrigger documentation
+*/
 gsap.utils.toArray("section").forEach((section, i) => {
   let background;
+    //apply image to the background of a section element
   if (section.querySelector(".bg_2")) {
     background = section.querySelector(".bg_2");
     background.style.backgroundImage = `url('Assets/images/library.jpg')`;
   }
 
-  // Give the backgrounds some random images
+  //make the image fade in once loaded
   background.style.opacity = 0;
   window.onload = function () { (gsap.to(background, { duration: 5, opacity: 100, ease: Power1.easeInOut })) };
   // Do the parallax effect on each section
   if (i) {
     background.style.backgroundPosition = `50% ${-innerHeight / 2}px`;
 
-    gsap.to(sbackground, {
+    gsap.to(background, {
       backgroundPosition: `50% ${innerHeight / 2}px`,
       ease: "none",
       scrollTrigger: {
@@ -23,7 +27,6 @@ gsap.utils.toArray("section").forEach((section, i) => {
     });
   }
 
-  // the first image should be positioned against the top. Use px on the animating part to work with GSAP. 
   else {
     background.style.backgroundPosition = "50% 0px";
 
@@ -40,14 +43,16 @@ gsap.utils.toArray("section").forEach((section, i) => {
   }
 });
 
-/*referenced & modified from https://css-tricks.com/writing-smarter-animation-code/ */
+/*referenced & modified from https://css-tricks.com/writing-smarter-animation-code/ 
+float up text and increase opacity as user scrolls
+*/
 if (document.querySelector(".panel1")) {
   function createPanel(panel) {
     var tl = new TimelineLite();
     tl.from(panel + " .text", { scrollTrigger: { trigger: panel + " .text", end: "top center", scrub: 1 }, y: 50, opacity: 0, ease: Power4.easeInOut })
       .staggerFrom(panel + " .text", 0.9, { y: 50, opacity: 0, ease: Power4.easeInOut }, 0.06);
 
-    return tl; //very important that the timeline gets returned
+    return tl; //return the timeline
   }
 
   var master = new TimelineMax();
